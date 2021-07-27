d370afb6e4d32e328758adb59025a54d1f02fc7b
Minimine <64435955+nsc-de@users.noreply.github.com>
2021-07-27 17:49:13 +0200

    🔍 Perform search using fuse.js on update search_contents input

--- src/main/www/scripts/search.ts
+++ src/main/www/scripts/search.ts
@@ -2,7 +2,7 @@ import Fuse from 'fuse.js'
 
 let searchIndex: Promise<Fuse<any>>|Fuse<any>|undefined;
 
-async function createSearchIndex() {
+async function createSearchIndex(): Promise<Fuse<any>> {
   
   // @ts-ignore
   const searchIndex =  await import("./search-index.json");
@@ -17,7 +17,7 @@ async function createSearchIndex() {
 
 }
 
-async function getSearchIndex() {
+async function getSearchIndex(): Promise<Fuse<any>> {
   
   if(!searchIndex) {
     return await new Promise((rs) => {
@@ -61,5 +61,15 @@ document.addEventListener('DOMContentLoaded', () => {
       search_bar.classList.remove("shown");
     }
   });
+
+  const search_contents = document.getElementById('search-contents') as HTMLInputElement;
+  async function change_listener() {
+    console.debug((await getSearchIndex()).search(search_contents.value))
+    // TODO Perform search
+  }
+  search_contents.addEventListener('keydown', change_listener);
+  search_contents.addEventListener('keyup', change_listener);
+  search_contents.addEventListener('keypress', change_listener);
+  search_contents.addEventListener('change', change_listener);
 
 });
