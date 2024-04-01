ed4b9673a762d233195ef7d7abebb92ebfcb86b3
Nicolas Schmidt <nicolas.schmidt@nscde.com>
2024-04-01 19:56:32 +0200

    Add docusaurus-lunr-search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -155,7 +155,10 @@ const config: Config = {
     },
   } satisfies Preset.ThemeConfig,
 
-  plugins: ["docusaurus-plugin-sass"],
+  plugins: [
+    "docusaurus-plugin-sass",
+    require.resolve("docusaurus-lunr-search"),
+  ],
 };
 
 export default config;
